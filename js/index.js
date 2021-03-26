30849c695326208b9c0d34c83fe770f0f70d866a
asbelov <asbelov2@gmail.com>
2021-03-26 15:20:44 +0400

    Изменения

--- js/index.js
+++ js/index.js
@@ -18,20 +18,26 @@ window.onload = async () => {
       document.getElementById("addbook__file").style.display = "block";
       document.getElementById("addbook__booktext").style.display = "none";
     }
-  };
-  /*addbook__submit.onclick = async function(e) {
-    let url="https://apiinterns.osora.ru/";
-    let text = document.getElementById("addbook__file").value; // our file with book
-    let name = document.getElementById("addbook__name").value;
-    let data = {
-      login: name,
-      file: text
+
+    addbook__submit.onclick = async function(e) {
+      let url="https://apiinterns.osora.ru";
+      let text = document.getElementById("addbook__file").files[0]; // our file with book
+      let name = document.getElementById("addbook__name").value;
+      let data = {
+        login: name,
+        file: text
+      }
+      console.log(data);
+      let response = await fetch(url, {
+        method: 'POST',
+        body: data
+      })
+      .then(res => res.json())
+      .then(data => console.log('+', data))
+      .catch((e) => {
+        console.log('Error: ' + e.message);
+        console.log(e.response);
+      })
     }
-    console.log(data);
-    let response = await fetch(url, {
-      method: 'POST',
-      body: data
-    });
-    console.log(file,response,response.json());
-  }*/
+  };
 };
