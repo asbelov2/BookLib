50582c145271a8a0b17c1591381f171d81353a9f
asbelov <asbelov2@gmail.com>
2021-03-29 17:29:23 +0400

    Получилось отправить удачных запрос

--- js/index.js
+++ js/index.js
@@ -19,26 +19,23 @@ window.onload = async () => {
       document.getElementById("addbook__booktext").style.display = "none";
     }
 
-    addbook__submit.onclick = async function(e) {
+    addbook__submit.onclick = async function (e) {
       e.preventDefault();
-      let url="https://apiinterns.osora.ru/";
-      let text = document.getElementById("addbook__file").files[0];
-      let name = document.getElementById("addbook__name").value;
-      let data = {
-        login: name,
-        file: text
-      }
-      console.log(data);
+      let url = "https://apiinterns.osora.ru/";
+      let bookForm = new FormData(document.forms.addbookform);
+      bookForm.append('login', 'anylogin');
+      bookForm.append('file', document.getElementById("addbook__file").files[0]);
+      
       let response = await fetch(url, {
         method: 'POST',
-        body: data
+        body: bookForm
       })
-      .then(res => res.json())
-      .then(data => console.log('+', data))
-      .catch((e) => {
-        console.log('Error: ' + e.message);
-        console.log(e.response);
-      })
-    }
+        .then((res) => res.text())
+        .then((data) => console.log("+", data))
+        .catch((e) => {
+          console.log("Error: " + e.message);
+          console.log(e.response);
+        });
+    };
   };
 };
