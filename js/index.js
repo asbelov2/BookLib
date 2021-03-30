094b95f299e206f0d91bbfd1e91ae7c22deadc40
asbelov <asbelov2@gmail.com>
2021-03-31 02:08:20 +0400

    сделал секцию любимых книг с drag&drop

--- js/index.js
+++ js/index.js
@@ -5,7 +5,14 @@ window.onload = async () => {
     localStorage.setItem("books", JSON.stringify([]));
   } else {
     books = JSON.parse(localStorage.getItem("books"));
+    sortBooks();
   }
+  document.getElementsByClassName(
+    "menu__item_addbook"
+  )[0].onclick = async function (e) {
+    document.getElementById("addbookform").style.display = "flex";
+    document.getElementsByClassName("bookview")[0].style.display = "none";
+  };
 
   renderBooks();
 
@@ -58,13 +65,25 @@ window.onload = async () => {
             type: "text/plain",
           }
         );
-        books.push({ name: file.name, text: await file.text() });
+        books.push({
+          name: file.name,
+          text: await file.text(),
+          time: Date.now(),
+          completed: false,
+          favorite: false,
+        });
         localStorage.setItem("books", JSON.stringify(books));
         bookForm.append("file", file);
       }
       if (rad[1].checked) {
         let file = document.getElementById("addbook__file").files[0];
-        books.push({ name: file.name, text: await file.text() });
+        books.push({
+          name: file.name,
+          text: await file.text(),
+          time: Date.now(),
+          completed: false,
+          favorite: false,
+        });
         localStorage.setItem("books", JSON.stringify(books));
         bookForm.append("file", file);
       }
@@ -85,51 +104,84 @@ window.onload = async () => {
 };
 
 function renderBooks() {
-  document.getElementsByClassName("booklist")[0].innerHTML = "";
+  document.getElementsByClassName("menu-booklist")[0].innerHTML = "";
+  document.getElementsByClassName("menu-favoritebooks")[0].innerHTML = "";
   for (let i = 0; i < books.length; ++i) {
     let bookItem = document.createElement("div");
     bookItem.id = "id" + i;
     document.querySelector("#book").content.querySelector("span").innerText =
       books[i].name;
-
     bookItem.innerHTML = document.getElementById("book").innerHTML;
-    document.getElementsByClassName("booklist")[0].appendChild(bookItem);
+    if (books[i].favorite === true) {
+      document.getElementsByClassName("menu-favoritebooks")[0].appendChild(bookItem);
+    } else {
+      document.getElementsByClassName("menu-booklist")[0].appendChild(bookItem);
+    }
 
+    if (books[i].completed === true) {
+      document
+        .querySelector("#id" + i)
+        .querySelector("li span").className = "book__title book_completed";
+    }
+
+    if (books[i].completed === false) {
+      document
+        .querySelector("#id" + i)
+        .querySelector("li span").className = "book__title book_uncompleted";
+    }
+
+    document.querySelector("#id" + i).draggable = true;
+    document.querySelector("#id" + i).ondragstart = dragStart;
+
+    //READ
     document
-      .querySelector(".booklist #id" + i)
+      .querySelector("#id" + i)
       .querySelector(
         ".book-buttons__button.book-buttons__button_read"
       ).onclick = async function (e) {
-      console.log("book_read");
-
+      document.getElementsByClassName("bookview")[0].style.display = "flex";
+      document.getElementById("addbookform").style.display = "none";
+      document.getElementsByClassName("bookview__bookname")[0].innerText =
+        books[i].name;
       document.getElementsByClassName("bookview__textarea")[0].value =
         books[i].text;
       document.getElementsByClassName("bookview__textarea")[0].readOnly = true;
     };
+    //DELETE
     document
-      .querySelector(".booklist #id" + i)
+      .querySelector("#id" + i)
       .querySelector(
         ".book-buttons__button.book-buttons__button_delete"
       ).onclick = async function (e) {
-      console.log("book_delete");
       books.splice(i, 1);
       localStorage.setItem("books", JSON.stringify(books));
       renderBooks();
     };
+
+    //COMPLETE
     document
-      .querySelector(".booklist #id" + i)
+      .querySelector("#id" + i)
       .querySelector(
         ".book-buttons__button.book-buttons__button_complete"
       ).onclick = async function (e) {
-      console.log("book_complete");
+      books[i].completed = true;
+      sortBooks();
+      localStorage.setItem("books", JSON.stringify(books));
+      renderBooks();
     };
 
+    //EDIT
     document
-      .querySelector(".booklist #id" + i)
+      .querySelector("#id" + i)
       .querySelector(
         ".book-buttons__button.book-buttons__button_edit"
       ).onclick = async function (e) {
-      console.log("book_edit");
+      document.getElementsByClassName("bookview")[0].style.display = "flex";
+      document.getElementById("addbookform").style.display = "none";
+      document.getElementsByClassName("bookview__bookname")[0].innerText =
+        books[i].name;
+      document.getElementsByClassName("bookview__textarea")[0].value =
+        books[i].text;
       document.getElementsByClassName("bookview__textarea")[0].readOnly = false;
 
       let save = document.createElement("input");
@@ -155,4 +207,51 @@ function renderBooks() {
       document.getElementsByClassName("bookview")[0].appendChild(cancel);
     };
   }
+}
+
+function sortBooks() {
+  books.sort(function (a, b) {
+    if (a.completed === b.completed) {
+      return a.time - b.time;
+    } else if (a.completed === true) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+}
+
+function dragStart(ev) {
+  ev.dataTransfer.effectAllowed = "move";
+  ev.dataTransfer.setData("Text", ev.target.getAttribute("id"));
+  ev.dataTransfer.setDragImage(ev.target, 100, 100);
+  return true;
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  return true;
+}
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragDropToFav(e) {
+  var data = e.dataTransfer.getData("Text");
+  books[data.match(/(\d+)/)[0]].favorite = true;
+  localStorage.setItem("books", JSON.stringify(books));
+  document.getElementsByClassName("menu-favoritebooks")[0].appendChild(document.getElementById(data));
+  renderBooks();
+  e.stopPropagation();
+  return false;
+}
+
+function dragDropToList(e) {
+  var data = e.dataTransfer.getData("Text");
+  books[data.match(/(\d+)/)[0]].favorite = false;
+  localStorage.setItem("books", JSON.stringify(books));
+  document.getElementsByClassName("menu-booklist")[0].appendChild(document.getElementById(data));
+  renderBooks();
+  e.stopPropagation();
+  return false;
 }
