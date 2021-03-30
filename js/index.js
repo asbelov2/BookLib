1dbab74af55eeaf4f365eac1e8d3fd59ebc4a315
asbelov <asbelov2@gmail.com>
2021-03-30 15:14:14 +0400

    Добавил хранение книг

--- js/index.js
+++ js/index.js
@@ -1,21 +1,38 @@
+let books = [];
+
 window.onload = async () => {
+  if (localStorage.getItem("books") === null) {
+    localStorage.setItem("books", JSON.stringify([]));
+  } else {
+    books = JSON.parse(localStorage.getItem("books"));
+  }
+
+  console.log(books);
   var rad = document.getElementsByName("loadtype");
   if (rad[0].checked) {
     document.getElementById("addbook__file").style.display = "none";
+    document.getElementById("addbook__namelabel").style.display = "block";
+    document.getElementById("addbook__name").style.display = "block";
     document.getElementById("addbook__booktext").style.display = "block";
   }
   if (rad[1].checked) {
     document.getElementById("addbook__file").style.display = "block";
+    document.getElementById("addbook__namelabel").style.display = "none";
+    document.getElementById("addbook__name").style.display = "none";
     document.getElementById("addbook__booktext").style.display = "none";
   }
   addbookform.onchange = function (e) {
     var rad = document.getElementsByName("loadtype");
     if (rad[0].checked) {
       document.getElementById("addbook__file").style.display = "none";
+      document.getElementById("addbook__namelabel").style.display = "block";
+      document.getElementById("addbook__name").style.display = "block";
       document.getElementById("addbook__booktext").style.display = "block";
     }
     if (rad[1].checked) {
       document.getElementById("addbook__file").style.display = "block";
+      document.getElementById("addbook__namelabel").style.display = "none";
+      document.getElementById("addbook__name").style.display = "none";
       document.getElementById("addbook__booktext").style.display = "none";
     }
 
@@ -23,12 +40,35 @@ window.onload = async () => {
       e.preventDefault();
       let url = "https://apiinterns.osora.ru/";
       let bookForm = new FormData(document.forms.addbookform);
-      bookForm.append('login', 'anylogin');
-      bookForm.append('file', document.getElementById("addbook__file").files[0]);
-      
+      bookForm.append("login", "anylogin");
+      let name = document.getElementById("addbook__name").value;
+      let text = document.getElementById("addbook__booktext").value;
+      console.log(books);
+      if (rad[0].checked) {
+        let file = new File(
+          [
+            new Blob([text], {
+              type: "text/plain",
+            }),
+          ],
+          name,
+          {
+            type: "text/plain",
+          }
+        );
+        books.push({'name':file.name, 'text':(await file.text())});
+        localStorage.setItem("books", JSON.stringify(books));
+        bookForm.append("file", file);
+      }
+      if (rad[1].checked) {
+        let file = document.getElementById("addbook__file").files[0];
+        books.push({'name':file.name, 'text':(await file.text())});
+        localStorage.setItem("books", JSON.stringify(books));
+        bookForm.append("file", file);
+      }
       let response = await fetch(url, {
-        method: 'POST',
-        body: bookForm
+        method: "POST",
+        body: bookForm,
       })
         .then((res) => res.text())
         .then((data) => console.log("+", data))
@@ -36,6 +76,7 @@ window.onload = async () => {
           console.log("Error: " + e.message);
           console.log(e.response);
         });
+      console.log(books);
     };
   };
 };
